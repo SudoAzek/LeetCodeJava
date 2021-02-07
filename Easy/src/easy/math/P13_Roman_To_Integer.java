c267cdef356501874da02d574d9721066e81321d
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-02-07 12:15:27 +0900

    Create P13_Roman_To_Integer.java

---
+++ Easy/src/easy/math/P13_Roman_To_Integer.java
@@ -0,0 +1,24 @@
+package easy.math;
+
+public class P13_Roman_To_Integer {
+    public static void main(String[] args) {
+        RomanToIntSolution romanToIntSolution = new RomanToIntSolution();
+        System.out.println(romanToIntSolution.romanToInt("III"));
+    }
+}
+
+class RomanToIntSolution {
+    public int romanToInt(String s) {
+        int num = 0;
+        for (int i = s.length() - 1; i >= 0 ; i--) {
+            if (s.charAt(i) == 'I'){
+                num++;
+            }
+
+            if(s.charAt(i) == 'V') {
+                System.out.print(5);
+            }
+        }
+        return num;
+    }
+}
