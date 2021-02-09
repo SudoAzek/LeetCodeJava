ade9ef1274a9becdbeab6f1945427276e6c415e8
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-02-09 09:20:15 +0900

    Create D07_ShortestDistanceToCharacter.java
    not finished

---
+++ Easy/src/daily_challenge/D07_ShortestDistanceToCharacter.java
@@ -0,0 +1,47 @@
+package daily_challenge;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.min;
+
+public class D07_ShortestDistanceToCharacter {
+    public static void main(String[] args) {
+        D07_ShortestDistanceToCharacterSolution d07_shortestDistanceToCharacterSolution = new D07_ShortestDistanceToCharacterSolution();
+        System.out.println(Arrays.toString(d07_shortestDistanceToCharacterSolution.shortestToChar("loveleetcode", 'e')));
+    }
+}
+
+/*
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        res = [0 if s[0] == c else float('inf')]
+
+        for i in range(1, n):
+            res.append(0 if s[i] == c else res[i - 1] + 1)
+
+        for i in reversed(range(n - 1)):
+            res[i] = min(res[i], res[i + 1] + 1)
+
+        return res
+ */
+
+
+class D07_ShortestDistanceToCharacterSolution {
+    public int[] shortestToChar(String s, char c) {
+        int l = s.length();
+        int[] ans = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            if (s.charAt(i) == c) {
+                ans[i] = 0;
+            } else {
+                ans[i] =+ i;
+            }
+        }
+        for (int i = l - 1; i > 0; i--) {
+            ans[i] = min(ans[i], ans[i] =+ 1);
+        }
+        return ans;
+    }
+}
