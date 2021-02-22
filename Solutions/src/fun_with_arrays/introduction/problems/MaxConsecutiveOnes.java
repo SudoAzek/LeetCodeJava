4cb5b1ee9745a1f4f0af3260c18d651127462816
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-02-22 10:50:17 +0900

    Update MaxConsecutiveOnes.java

--- Solutions/src/fun_with_arrays/introduction/problems/MaxConsecutiveOnes.java
+++ Solutions/src/fun_with_arrays/introduction/problems/MaxConsecutiveOnes.java
@@ -3,30 +3,54 @@ package fun_with_arrays.introduction.problems;
 public class MaxConsecutiveOnes {
     public static void main(String[] args) {
         MaxConsecutiveOnesSolution sol = new MaxConsecutiveOnesSolution();
-        System.out.println(sol.findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1}));
+        System.out.println(sol.findMaxConsecutiveOnes(new int[]{1, 1, 0, 1}));
     }
 }
 
 class MaxConsecutiveOnesSolution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxx = 0;
-        int maxy = 0;
-        int a = nums[0];
-
-        int i = 0;
-        while (i < nums.length) {
-            if(nums[i] == 1) {
-                maxx++;
-                maxy = maxx;
-            } else {
-                maxx = 0;
-            }
-            i++;
+        int count = 0;
+        int max = 0;
+        for (int num : nums) {
+            if (num == 1) count++;
+            else count = 0;
+            if (max < count) max = count;
         }
-//
-//        for (int i = 1; i < nums.length; i++) {
-//
-//        }
-        return maxy;
+        return max;
+    }
+}
+
+class MaxConsecutiveOnesSolution_1 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int curr = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1)
+                curr++;
+            else
+                curr = 0;
+            if (curr > max)
+                max = curr;
+        }
+        return max;
+    }
+}
+
+class MaxConsecutiveOnesSolution_2 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (max < count) {
+                max = count;
+            }
+        }
+        return max;
     }
 }
