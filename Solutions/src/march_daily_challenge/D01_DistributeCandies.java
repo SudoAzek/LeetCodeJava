bea876f9e9c84cebdd623d671c9c1cfa38a8c10e
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-03-02 10:47:04 +0900

    Update D01_DistributeCandies.java

--- Solutions/src/march_daily_challenge/D01_DistributeCandies.java
+++ Solutions/src/march_daily_challenge/D01_DistributeCandies.java
@@ -21,3 +21,19 @@ class DistributeCandiesSolution {
         return Math.min(candyType.length / 2, distinctCandies.size());
     }
 }
+
+class DistributeCandiesSolution2 {
+    public int distributeCandies(int[] candies) {
+        int candiesToEat = candies.length / 2;
+        int typeCount = 0;
+        boolean[] typeUsed = new boolean[200001];
+        for (int c: candies) {
+            int t = c + 100000;
+            if (!typeUsed[t]) {
+                if (++typeCount >= candiesToEat)  return candiesToEat;
+                typeUsed[t] = true;
+            }
+        }
+        return typeCount;
+    }
+}
