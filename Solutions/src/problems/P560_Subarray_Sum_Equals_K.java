62757ab810e9291e6997058689177110e3e26962
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2023-07-08 15:18:08 -0400

    Create P560_Subarray_Sum_Equals_K.java

---
+++ Solutions/src/problems/P560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,53 @@
+package problems;
+
+public class P560_Subarray_Sum_Equals_K {
+    public static void main(String[] args) {
+        System.out.println(subarraySum2(new int[] {1, 2, 3}, 3));
+        System.out.println(subarraySum3(new int[] {1, 2, 3}, 3));
+    }
+
+    static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++) {
+                    sum += nums[i];
+                }
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    static int subarraySum2(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k) count++;
+            }
+        }
+        return count;
+    }
+
+    static int subarraySum3(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
