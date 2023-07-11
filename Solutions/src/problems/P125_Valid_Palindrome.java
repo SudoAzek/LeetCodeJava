e3e5524a773daa6ec180fefa965a148ddccc0de8
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2023-07-11 05:41:46 -0400

    Update P125_Valid_Palindrome.java

--- Solutions/src/problems/P125_Valid_Palindrome.java
+++ Solutions/src/problems/P125_Valid_Palindrome.java
@@ -39,6 +39,22 @@ public class P125_Valid_Palindrome {
         return result.toString();
     }
 
+    public static boolean isPalindrome2(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 //    public static String removeNonAlphanumerics(String s) {
 //        Pattern patter = Pattern.compile("[^a-zA-Z0-9]");
 //        String result = patter.matcher(s).replaceAll("");
